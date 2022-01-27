eebeca7a6fdcd3d1d07c49b04acdd9f49a05e9ed
Yue <cathywang428@gmail.com>
2022-01-27 16:03:09 -0600

    install debugger

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask_debugtoolbar import DebugToolbarExtension
 from stories import stories
 
 app = Flask("__name__")
-# app.config['SECRET_KEY']="123"
-# debug = DebugToolbarExtension(app)
+app.config['SECRET_KEY']="123"
+debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def home():
